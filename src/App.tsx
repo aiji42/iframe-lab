86f449812e56814190fd8590e7161a874605f917
Aiji Uejima <aiji-uejima@cybozu.co.jp>
2025-01-30 00:42:15 +0900

    misc update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => {
         <textarea
           onChange={(e) => setIframeInner(e.target.value)}
           defaultValue={iframeInner}
-          className="w-[600px] p-2 border rounded-sm"
+          className="w-[600px] p-2 border rounded-sm hidden sm:block"
           autoFocus
         />
 
@@ -67,7 +67,6 @@ const defaultIframeInner = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>iframe</title>
 </head>
